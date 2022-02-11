18727e9e833ddc00dee36edc5ad797afe09a1cc1
wurtman <46197673+wurtman@users.noreply.github.com>
2022-02-11 09:43:01 -0800

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-            if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
-                chrome.tabs.insertCSS(null, {file: './mystyles.css'});
-                chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('I injected'))
-        }
-    });
-});
